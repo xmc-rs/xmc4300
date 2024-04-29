58fe20e71d91f975d30891ded496dfd4a2cee704
Lucas Brendel <lucasbrendel@gmail.com>
2024-04-28 23:01:41 -0500

    chore: adding argument to keep legact formatting

--- svd.sh
+++ svd.sh
@@ -3,7 +3,7 @@
 #     cargo install -f svd2rust
 #     cargo install -f form
 rm -r src/
-svd2rust -i XMC4300.svd
+svd2rust -i XMC4300.svd --ident-formats-theme legacy
 mkdir src
 form -i lib.rs -o src/
 cargo fmt
